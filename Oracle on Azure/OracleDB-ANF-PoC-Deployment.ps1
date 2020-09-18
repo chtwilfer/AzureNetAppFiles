d1c7b9ad12e19179809dda9e7106475f3a5795c0
Christian Twilfer <christian.twilfer@outlook.de>
2020-09-18 08:36:16 +0200

    Create OracleDB-ANF-PoC-Deployment.ps1

---
+++ Oracle on Azure/OracleDB-ANF-PoC-Deployment.ps1
@@ -0,0 +1,19 @@
+<#
+	.SYNOPSIS
+	Create Resources in Azure for Oracle on Azure with Azure NetApp Files (ANF) in Azure
+
+	.DESCRIPTION
+        		
+	
+	.NOTES
+	Version:	0.1
+	Author: 	Orange Networks GmbH	- Christian Twilfer (c.twilfer@orangenet.de)
+	
+	Creation Date: 18.09.2020
+        Purpose / Changes:
+           
+		
+	.PARAMETER
+
+             
+#>
