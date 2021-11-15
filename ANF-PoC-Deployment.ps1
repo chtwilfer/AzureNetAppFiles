3e61bed4f8ee0fca08e6cc23ba757ee49535f992
Twilfi <christian.twilfer@outlook.de>
2021-11-15 14:09:08 +0100

    Update ANF-PoC-Deployment.ps1

--- ANF-PoC-Deployment.ps1
+++ ANF-PoC-Deployment.ps1
@@ -13,7 +13,7 @@
 	
 	.NOTES
 	Version:	1.6
-	Author: 	Orange Networks GmbH	- Christian Twilfer (c.twilfer@orangenet.de)
+	Author: 	Christian Twilfer (christian.twilfer@outlook.de)
 	
 	Creation Date: 29.05.2020
         Purpose / Changes:
